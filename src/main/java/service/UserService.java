71ba35847e8b4947d8ca3bae1811a2e95f79edf1
chepaskov <chepaksov@mail.ru>
2020-01-10 16:40:08 +0300

    asdasdasdasawd23awdaw21klawdawdasdasawdawdawd123sgadwsdfds

--- src/main/java/service/UserService.java
+++ src/main/java/service/UserService.java
@@ -17,20 +17,22 @@ public class UserService {
 
     private static UserService userService;
 
-    private SessionFactory sessionFactory;
-
-    public UserService(SessionFactory sessionFactory) {
-        this.sessionFactory = sessionFactory;
-    }
 
     public static UserService getInstance() {
         if (userService == null) {
-            userService = new UserService(DBHelper.getSessionFactory());
+            userService = new UserService();
         }
         return userService;
     }
+
+
+
+    public SessionFactory getSesFac() {
+        return DBHelper.getInstance().getSessionFactory();
+
+    }
     public UserService() {
-        this.userDAO = new UserHibernateDAO(sessionFactory.openSession());
+        this.userDAO = new UserHibernateDAO(getSesFac().openSession());
     }
 
 
@@ -67,30 +69,7 @@ public class UserService {
     }
 
 
-    private static Connection getMysqlConnection() {
-        try {
-            DriverManager.registerDriver((Driver) Class.forName("com.mysql.cj.jdbc.Driver").newInstance());
 
-            StringBuilder url = new StringBuilder();
-
-            url.
-                    append("jdbc:mysql://").        //db type
-                    append("localhost:").           //host name
-                    append("3306/").                //port
-                    append("db_example?").          //db name
-                    append("user=root&").          //login
-                    append("password=root").       //password
-                    append("&serverTimezone=UTC");   //setup server time
-
-            System.out.println("URL: " + url + "\n");
-
-            Connection connection = DriverManager.getConnection(url.toString());
-            return connection;
-        } catch (SQLException | InstantiationException | IllegalAccessException | ClassNotFoundException e) {
-            e.printStackTrace();
-            throw new IllegalStateException();
-        }
-    }
 
 
 }
--- src/main/java/util/DBHelper.java
+++ src/main/java/util/DBHelper.java
@@ -8,17 +8,37 @@ import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.service.ServiceRegistry;
 
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 
 public class DBHelper {
 
+    public static DBHelper instance;
+
+    private DBHelper() {
+    }
+
+
+    public static DBHelper getInstance() {
+        if (instance == null) {
+            instance = new DBHelper();
+        }
+        return instance;
+    }
+
+
     private static SessionFactory sessionFactory;
 
-    public static SessionFactory getSessionFactory() {
+    public SessionFactory getSessionFactory() {
         if (sessionFactory == null) {
             sessionFactory = createSessionFactory();
         }
         return sessionFactory;
     }
+
 
     @SuppressWarnings("UnusedDeclaration")
     private static Configuration getMySqlConfiguration() {
@@ -27,7 +47,7 @@ public class DBHelper {
 
 
         configuration.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQLDialect");
-        configuration.setProperty("hibernate.connection.driver_class", "com.mysql.jdbc.Driver");
+        configuration.setProperty("hibernate.connection.driver_class", "com.mysql.cj.jdbc.Driver");
         configuration.setProperty("hibernate.connection.url", "jdbc:mysql://localhost:3306/db_example?serverTimezone=UTC");
         configuration.setProperty("hibernate.connection.username", "root");
         configuration.setProperty("hibernate.connection.password", "root");
@@ -46,6 +66,29 @@ public class DBHelper {
     }
 
 
+    public Connection getConnection() {
+        try {
+            DriverManager.registerDriver((Driver) Class.forName("com.mysql.cj.jdbc.Driver").newInstance());
 
+            StringBuilder url = new StringBuilder();
+
+            url.
+                    append("jdbc:mysql://").        //db type
+                    append("localhost:").           //host name
+                    append("3306/").                //port
+                    append("db_example?").          //db name
+                    append("user=root&").          //login
+                    append("password=root").       //password
+                    append("&serverTimezone=UTC");   //setup server time
+
+            System.out.println("URL: " + url + "\n");
+
+            Connection connection = DriverManager.getConnection(url.toString());
+            return connection;
+        } catch (InstantiationException | IllegalAccessException | ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+            throw new IllegalStateException();
+        }
+    }
 
 }
