895fa692a5091852bd28d2ada9b408df0e567c1f
chepaskov <chepaksov@mail.ru>
2020-01-14 23:53:33 +0300

    1

--- src/main/java/AppConfig.java
+++ src/main/java/AppConfig.java
@@ -1,5 +1,6 @@
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 
 @Configuration
 public class AppConfig {
@@ -10,4 +11,15 @@ public class AppConfig {
         helloWorld.setMessage("Hello World!");
         return helloWorld;
     }
+
+
+    @Bean(name="cat")
+    @Scope(value = "prototype")
+    public Cat getCat() {
+        Cat cat = new Cat();
+        cat.setName("Murka");
+        return cat;
+    }
+
+
 }
