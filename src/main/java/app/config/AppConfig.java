50b5294653926514d38502ddc79fc87373202e5f
chepaskov <chepaksov@mail.ru>
2020-01-16 13:59:50 +0300

    1

--- src/main/java/app/config/AppConfig.java
+++ src/main/java/app/config/AppConfig.java
@@ -1,14 +1,20 @@
 package app.config;
 
+import app.model.Dog;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+
 
 @Configuration
 @ComponentScan(basePackages = "app")
 public class AppConfig {
     @Bean(name = "Dog")
-    public void getDog() {
+    public Dog getDog() {
+        return new Dog();
 
     }
 }
