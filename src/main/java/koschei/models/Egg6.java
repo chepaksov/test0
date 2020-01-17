4fcbb16dad756143ff94069acd016b59e21e6303
chepaskov <chepaksov@mail.ru>
2020-01-17 21:47:28 +0300

    1

--- src/main/java/koschei/models/Egg6.java
+++ src/main/java/koschei/models/Egg6.java
@@ -2,8 +2,16 @@ package koschei.models;
 
 public class Egg6 {
 
+    private Needle7 needle7;
+
+    public Egg6(Needle7 needle7) {
+        this.needle7 = needle7;
+    }
+
+
+
     @Override
     public String toString() {
-        return ", в яйце иголка " + "";
+        return "в утке — яйцо, " + needle7.toString();
     }
 }
