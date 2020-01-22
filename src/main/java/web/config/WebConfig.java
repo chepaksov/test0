edbf544a1b34e2ab21e5fef42baa9dfaea693178
chepaskov <chepaksov@mail.ru>
2020-01-22 18:09:58 +0300

    1

--- src/main/java/web/config/WebConfig.java
+++ src/main/java/web/config/WebConfig.java
@@ -24,7 +24,7 @@ import org.springframework.web.servlet.view.JstlView;
 @ComponentScan("web")
 public class WebConfig implements WebMvcConfigurer {
 
-    /*
+
 
     @Bean(name = "messageSource")
     public MessageSource getMessageResource() {
@@ -51,7 +51,7 @@ public class WebConfig implements WebMvcConfigurer {
 
         registry.addInterceptor(localeInterceptor).addPathPatterns("/*");
     }
-*/
+
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
